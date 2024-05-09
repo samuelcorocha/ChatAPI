a2e6402c3483421b0943584f87428dcbb5c40e48
Luiz <lmartinsbeleza@gmail.com>
2024-05-09 17:29:58 -0400

    Create function to login and show user by id

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,25 +11,70 @@ class UsersController extends Controller
 {
     protected $usermd;
 
-    public function __construct(User $user)
-    {
+    public function __construct(User $user) {
         $this->usermd = $user;
     }
 
-    public function storeUser(StoreUserRequest $request)
-    {
-        // The incoming request is valid...
+    public function storeUser(StoreUserRequest $request) {
+        try{
+            $user = $this->usermd->create([
+                'name'=>$request->username,
+                'password'=>bcrypt($request->password),
+                'email'=>$request->email,
+                'access_token'=>uuid_create('4')
+            ]);
+            return response()->json([
+              'success' => 'Usuário criado com sucesso',
+              'user' => $user
+            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }catch(\Exception $e){
+            return response()->json([
+              'error' => $e->getMessage()
+            ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+    }
 
-        // Retrieve the validated input data...
-        $validated = $request->validated();
+    public function login(LoginRequest $request) {
+        auth()->logout();
+        if(!(isset($request->username) || isset($request->email)))
+            return response()->json([
+                'error' => 'Informe o Usuário ou o Email para efetuar o login'
+            ]);
 
-        dd($request, $request->username, uuid_create('4'));
-        $user = $this->usermd->create([
-            'name'=>$request->username,
-            'password'=>bcrypt($request->password),
-            'email'=>$request->email,
-            'access_token'=>uuid_create('4')
-        ]);
-        return response()->json($user, 200);
+        if(
+            auth()->attempt(['name' => $request->username, 'password' => $request->password]) ||
+            auth()->attempt(['email' => $request->email, 'password' => $request->password])
+        ){
+            if(!(auth()->id() == 1 || auth()->id() == 2)){
+                dd('para');
+                auth()->user()->update([
+                    'access_token' => uuid_create()
+                ]);
+            }
+            return response()->json([
+                'success' => 'Bem-vindo ao ChatApi '.auth()->user()->name.', para acessar as informações utilize o seguinte token para o acesso',
+                'acces_token' => auth()->user()->access_token
+            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+        return response()->json(['error' => 'Usuario não encontrado'], 400);
+    }
+
+    public function show($user_id) {
+        try{
+            $user = $this->usermd->where('id', $user_id)->first();
+            if(!$user)
+                return response()->json([
+                    'error' => 'Usuário não encontrado, verifique se o usuário está correto.'
+                ], 400);
+
+            return response()->json([
+                'success' => 'Usuário encontrado',
+                'user' => $user
+            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->controller(UsersController::class)->group(function(){
     Route::post('/', 'storeUser');
-    Route::post('/login');
-    Route::get('/{user_id}');
+    Route::post('/login', 'login');
+    Route::get('/{user_id}', 'show');
 });
 
 Route::prefix('rooms')->controller('')->group(function(){
