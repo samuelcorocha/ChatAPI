657a559794f8d247ec5491a961208ff537564271
Luiz <lmartinsbeleza@gmail.com>
2024-05-10 18:56:52 -0400

    Finalizando função de envio de mensagem privada

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Messages;
+use App\Models\Room;
+use App\Models\UserRoom;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-  protected $messagesmd;
-  public function __construct(Messages $messages){
-    $this->messagesmd = $messages;
+  protected $messagesMD;
+  protected $roomMD;
+  protected $userRoomMD;
+  public function __construct(Messages $messages, Room $room, UserRoom $userRoom){
+    $this->messagesMD = $messages;
+    $this->roomMD = $room;
+    $this->userRoomMD = $userRoom;
   }
 
   public function getMessage(Request $request, $receiver_id = 0) {
@@ -21,8 +27,9 @@ class MessagesController extends Controller
             'error' => 'Erro ao tentar enviar a messagem, tente novamente mais tarde'
           ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
       }
+      $room = $this->roomMD->where('name', 'receiver_'.$receiver_id.'_'.auth()->id())->first();
+      $messages = $this->messagesMD->findAllByRoom($room->id);
 
-      $messages = $this->messagesmd->findAllByRoom($receiver_id);
       return response()->json($messages, 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }catch (\Exception $e){
       return response()->json([
@@ -33,12 +40,51 @@ class MessagesController extends Controller
 
   public function sendMessage($message, $receiver_id) {
     try {
-      if ($message == '' || is_null($message)) return;
+      if ($message == '' || is_null($message)) return true;
 
-      $this->messagesmd->create([
+      $room = $this->roomMD->where('name', 'receiver_'.$receiver_id.'_'.auth()->id())->first();
+      if(!$room){
+        $res = $this->createPrivateChat($receiver_id);
+        if(!$res) return false;
+
+        $room = $this->roomMD->where('name', 'receiver_'.$receiver_id.'_'.auth()->id())->first();
+      }
+
+      $this->messagesMD->create([
         'message' => $message,
-        'room_id' => $receiver_id,
+        'room_id' => $room->id,
         'user_id' => auth()->id()
+      ]);
+      return true;
+    }catch(\Exception $e){
+      return false;
+    }
+  }
+
+  public function createPrivateChat($receiver_id){
+    try {
+      $room = $this->roomMD->create([
+        'name' => 'receiver_'.$receiver_id.'_'.auth()->id(),
+        'limit' => 2
+      ]);
+      $res = $this->createConnectionPrivate($room->id, $receiver_id);
+      if(!$res) return false;
+
+      return true;
+    }catch(\Exception $e){
+      return false;
+    }
+  }
+
+  public function createConnectionPrivate($room_id, $receiver_id) {
+    try {
+      $this->userRoomMD->create([
+        'user_id' => $receiver_id,
+        'room_id' => $room_id
+      ]);
+      $this->userRoomMD->create([
+        'user_id' => auth()->id(),
+        'room_id' => $room_id
       ]);
       return true;
     }catch(\Exception $e){
