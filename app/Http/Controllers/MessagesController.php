bd718c786a754edbb581ee76b92a28a0c97df4c8
Luiz <lmartinsbeleza@gmail.com>
2024-05-09 22:19:40 -0400

    Adicionando arquivos de messages, para enviar messagens para um usuario unico e retornar as messagens enviadas

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Messages;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+  protected $messagesmd;
+  public function __construct(Messages $messages){
+    $this->messagesmd = $messages;
+  }
+
+  public function getMessage(Request $request, $receiver_id = 0) {
+    try{
+      if(isset($request->message)){
+        $mes = $this->sendMessage($request->message, $receiver_id);
+        if(!$mes)
+          return response()->json([
+            'error' => 'Erro ao tentar enviar a messagem, tente novamente mais tarde'
+          ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }
+
+      $messages = $this->messagesmd->findAllByRoom($receiver_id);
+      return response()->json($messages, 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }catch (\Exception $e){
+      return response()->json([
+        'error' => $e->getMessage()
+      ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }
+  }
+
+  public function sendMessage($message, $receiver_id) {
+    try {
+      if ($message == '' || is_null($message)) return;
+
+      $this->messagesmd->create([
+        'message' => $message,
+        'room_id' => $receiver_id,
+        'user_id' => auth()->id()
+      ]);
+      return true;
+    }catch(\Exception $e){
+      return false;
+    }
+  }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,65 +16,64 @@ class UsersController extends Controller
     }
 
     public function storeUser(StoreUserRequest $request) {
-        try{
-            $user = $this->usermd->create([
-                'name'=>$request->username,
-                'password'=>bcrypt($request->password),
-                'email'=>$request->email,
-                'access_token'=>uuid_create('4')
-            ]);
-            return response()->json([
-              'success' => 'Usuário criado com sucesso',
-              'user' => $user
-            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        }catch(\Exception $e){
-            return response()->json([
-              'error' => $e->getMessage()
-            ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        }
+      try{
+        $user = $this->usermd->create([
+          'name'=>$request->username,
+          'password'=>bcrypt($request->password),
+          'email'=>$request->email,
+          'access_token'=>uuid_create('4')
+        ]);
+        return response()->json([
+          'success' => 'Usuário criado com sucesso',
+          'user' => $user
+        ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }catch(\Exception $e){
+        return response()->json([
+          'error' => $e->getMessage()
+        ], 400, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }
     }
 
     public function login(LoginRequest $request) {
-        auth()->logout();
-        if(!(isset($request->username) || isset($request->email)))
-            return response()->json([
-                'error' => 'Informe o Usuário ou o Email para efetuar o login'
-            ]);
+      auth()->logout();
+      if(!(isset($request->username) || isset($request->email)))
+        return response()->json([
+          'error' => 'Informe o Usuário ou o Email para efetuar o login'
+        ]);
 
-        if(
-            auth()->attempt(['name' => $request->username, 'password' => $request->password]) ||
-            auth()->attempt(['email' => $request->email, 'password' => $request->password])
-        ){
-            if(!(auth()->id() == 1 || auth()->id() == 2)){
-                dd('para');
-                auth()->user()->update([
-                    'access_token' => uuid_create()
-                ]);
-            }
-            return response()->json([
-                'success' => 'Bem-vindo ao ChatApi '.auth()->user()->name.', para acessar as informações utilize o seguinte token para o acesso',
-                'acces_token' => auth()->user()->access_token
-            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      if(
+        auth()->attempt(['name' => $request->username, 'password' => $request->password]) ||
+        auth()->attempt(['email' => $request->email, 'password' => $request->password])
+      ){
+        if(!(auth()->id() == 1 || auth()->id() == 2)){
+          $this->usermd->where('id', auth()->id())->first()->update([
+            'access_token' => uuid_create()
+          ]);
         }
-        return response()->json(['error' => 'Usuario não encontrado'], 400);
+        return response()->json([
+          'success' => 'Bem-vindo ao ChatApi '.auth()->user()->name.', para acessar as informações utilize o seguinte token para o acesso',
+          'acces_token' => auth()->user()->access_token
+        ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }
+      return response()->json(['error' => 'Usuario não encontrado'], 400);
     }
 
     public function show($user_id) {
-        try{
-            $user = $this->usermd->where('id', $user_id)->first();
-            if(!$user)
-                return response()->json([
-                    'error' => 'Usuário não encontrado, verifique se o usuário está correto.'
-                ], 400);
+      try{
+        $user = $this->usermd->where('id', $user_id)->first();
+        if(!$user)
+          return response()->json([
+              'error' => 'Usuário não encontrado, verifique se o usuário está correto.'
+          ], 400);
 
-            return response()->json([
-                'success' => 'Usuário encontrado',
-                'user' => $user
-            ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        }catch(\Exception $e){
-            return response()->json([
-                'error' => $e->getMessage()
-            ], 400);
-        }
+        return response()->json([
+          'success' => 'Usuário encontrado',
+          'user' => $user
+        ], 200, [], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+      }catch(\Exception $e){
+        return response()->json([
+          'error' => $e->getMessage()
+        ], 400);
+      }
     }
 }
